db92c7755196ffef5211d14419dbe1c566ffc1bd
Khaled85abo <khaledabou1985@gmail.com>
2022-05-06 13:48:19 +0200

    Implement Server rendered app
    - Add ejs

--- sockets/server.js
+++ sockets/server.js
@@ -1,11 +1,79 @@
 const express = require("express");
+const cors = require("cors");
+
 const http = require("http");
 const { Server } = require("socket.io");
-
+const {
+  userJoin,
+  getCurrentUser,
+  userLeave,
+  getRoomUsers,
+} = require("./utilities/users");
 const app = express();
+app.use(cors());
 
 const server = http.createServer(app);
 
-const io = new Server(server);
+// const io = new Server(server);
+
+const io = require("socket.io")(server, {
+  cors: {
+    origin: "http://localhost:8080",
+    allowedHeaders: ["my-custom-header"],
+    methods: ["GET", "POST", "DELETE", "PATCH"],
+    credentials: true,
+  },
+});
+
+io.on("connection", (socket) => {
+  console.log("user connected");
+  socket.on("joinRoom", ({ username, room }) => {
+    const user = userJoin(socket.id, username, room);
+
+    socket.join(user.room);
+
+    // Welcome current user
+    socket.emit("message", formatMessage(botName, "Welcome to ChatCord!"));
+
+    // Broadcast when a user connects
+    socket.broadcast
+      .to(user.room)
+      .emit(
+        "message",
+        formatMessage(botName, `${user.username} has joined the chat`)
+      );
+
+    // Send users and room info
+    io.to(user.room).emit("roomUsers", {
+      room: user.room,
+      users: getRoomUsers(user.room),
+    });
+  });
+
+  // Listen for chatMessage
+  socket.on("chatMessage", (msg) => {
+    const user = getCurrentUser(socket.id);
+
+    io.to(user.room).emit("message", formatMessage(user.username, msg));
+  });
+
+  // Runs when client disconnects
+  socket.on("disconnect", () => {
+    const user = userLeave(socket.id);
+
+    if (user) {
+      io.to(user.room).emit(
+        "message",
+        formatMessage(botName, `${user.username} has left the chat`)
+      );
+
+      // Send users and room info
+      io.to(user.room).emit("roomUsers", {
+        room: user.room,
+        users: getRoomUsers(user.room),
+      });
+    }
+  });
+});
 
 module.exports = { express, server, io, app };
